25aee3dc8460fd75d9741df149c09e00bf1300e3
rokamune <reynaoaguirre@gmail.com>
2025-03-26 16:53:31 -0700

    card date and game logic set up for memory match game
    - only duplicated cards array
    - shuffled cards array

--- food-jellycat-collection/src/MemoryGame.jsx
+++ food-jellycat-collection/src/MemoryGame.jsx
@@ -1,12 +1,62 @@
 import './MemoryGame.css';
+import JellycatCard from './JellycatCard';
+import { useState, useEffect } from 'react';
+
+const jellycatCards = [
+    { name: "cinnamon bun", type: "pastry", img: "/jellycat-images/cinnamon_bun.png" },
+    { name: "lemon", type: "fruit", img: "/jellycat-images/lemon.png" },
+    { name: "ginger", type: "veggie", img: "/jellycat-images/ginger.png" },
+    { name: "brie", type: "other", img: "/jellycat-images/brie.png"},
+    { name: "chili", type: "veggie", img: "/jellycat-images/chili.png"},
+    { name: "macaroon", type: "pastry", img: "/jellycat-images/macaroon.png"},
+    { name: "clementine", type: "fruit", img: "/jellycat-images/clementine.png"},
+    { name: "birthday cake", type: "pastry", img: "/jellycat-images/birthday_cake.png"},
+    { name: "cauliflower", type: "veggie", img: "/jellycat-images/cauliflower.png"},
+    { name: "lemon tart", type: "pastry", img: "/jellycat-images/lemon_tart.png"},
+    { name: "pickle", type: "veggie", img: "/jellycat-images/pickle.png"},
+];
 
 const MemoryGame = () => {
+
+    const [jellycats, setJellycats] = useState([]);
+
+    useEffect (() => {
+        const duplicateCards = [...jellycatCards, ...jellycatCards];
+        const shuffledCards = duplicateCards.sort(() => Math.random() - 0.5); 
+
+        setJellycats(shuffledCards);
+
+
+        // debugging
+        console.log(`cards: ${shuffledCards.length > 0 ? duplicateCards.length : "none"}`)
+    }, []);
+    
+
+
+
     return (
+
         <div className="game-container animated-item">
             <div className="page-header">
                 <h2>memory game</h2>
-                <h3>( ੭ ･ᴗ･ )੭</h3>
                 <p>flip two cards that are matching pairs until all cards are gone !</p>
+            </div>
+
+            <div className="grid">
+                { jellycats.length > 0 ? (
+                    jellycats.map((jellycat, index) => {
+                        <JellycatCard 
+                            key={index}
+                            name={jellycat.name}
+                            type={jellycat.type}
+                            img={jellycat.img}
+                        />
+                    })
+
+                ) : (
+                    <p>no jellycats yet!</p>  
+                )
+                }
             </div>
         </div>
     );
